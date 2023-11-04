5a004885fe8bef8c1b632a6c88cede901136e1af
Bianca Lemes Costa <112573642+bialemess@users.noreply.github.com>
2023-11-04 14:32:47 -0300

    Update relatorio.php
    erro na proporção

--- relatorio.php
+++ relatorio.php
@@ -32,7 +32,41 @@ if (isset($_SESSION['username']) && null !== $_SESSION['level']) {
 <head>
   <title>Relatório | AlmoxariSars</title>
 
-  </style>
+  
+  <style>
+        body {
+            margin: 0;
+            overflow: hidden;
+        }
+
+        .iframe-container {
+          margin-top: 40px;
+          margin-right: 90px;
+            width: 100vw;
+            height: 100vh;
+            overflow: hidden;
+          
+        }
+
+        iframe {
+          margin-top: 100px;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%; 
+            height: 100%; 
+            border: none;
+            transform-origin: 0 0;
+            
+            margin-right: 10px;
+        
+        }
+
+
+        
+    </style>
+
+
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   <link rel="stylesheet" href="style/styleteste7.css">
   <link rel="icon" type="image/png" href="logo/1.png">
@@ -43,45 +77,29 @@ if (isset($_SESSION['username']) && null !== $_SESSION['level']) {
   include 'menuLateral.php';
 
   ?>
-  <!--
-  <br><br>
-  <div class="divRelatorio1">
-	<form>
-		<div class="titleRelatorio">
-		<h1 >Relatório</h1>
-</div>
-		<div class="divTextRelatorio1">
-		<label for="nome">Id do usuário:</label>
 
-		<input type="text" id="nome" name="nome" placeholder="Digite seu nome" required  class="formaticTextRelatorio">
-    <label for="relatorio">Especificações:</label>
-		<select id="relatorio" name="relatorio" class="formaticRelatorio">
-		<option value="">Selecione um filtro</option>
-			<option value="mensal">Hidraulica</option>
-			<option value="semestral">Elétrica</option>
-      <option value="semestral">Cabeamento</option>
-      <option value="semestral">Estrutura (Aço,concreto,alvenaria...)</option>
-      <option value="semestral">Cobertura</option>
-      <option value="semestral">Impermeabilidade</option>
-      <option value="semestral">SPDA</option>
-      <option value="semestral">Pintura</option>
-      <option value="semestral">Climatização</option>
-      <option value="semestral">Pavimentação</option>
+<div class="divRelatorio1">
+    <div class="titleRelatorio">
+      <h1>Relatório</h1>
+    </div>
+    <br>
+
+
+  
+
+<div class="iframe-container">
+    <iframe title="relatorio" src="https://app.powerbi.com/reportEmbed?reportId=6662da05-e53d-4b46-9e69-1f693bb3d0f1&autoAuth=true&ctid=d3acd233-2d54-401d-8cc3-d86d8614b7c5" frameborder="0" allowFullScreen="true"
       
-		</select>
-		
-		<label for="Periodo">Periodo:</label>
-		<select id="Periodo" name="Periodo"  class="formaticRelatorio">
-			<option value="mensal">Mensal</option>
-			<option value="semestral">Semestral</option>
-		</select>
-		<a href="novoRelatorio.php" type="submit" class="btnRelatorio2">Visualizar
-		</a>
-		
+      style="width: 95%; height: 84%;"></iframe>
+      <!-- ele "tem" que ficar desse jeito p manter filtro e página escondido.  esse aqui muda diretamente no tamanho de exibição, os outros sao tentativas-->
 </div>
-	</form>
 </div>
--->
+
+
+
+
+
+
 
   <footer class="footer">
     <footer>
